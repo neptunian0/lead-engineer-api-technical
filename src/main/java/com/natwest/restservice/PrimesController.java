13ad20fbae8b6fe6ef323971e667491b84e2154a
Lloyd Tucker <lloyd.tucker@bluebank.io>
2021-04-02 11:32:44 +0100

    Add Sieve of Eratosthenes algorithm

--- src/main/java/com/natwest/restservice/PrimesController.java
+++ src/main/java/com/natwest/restservice/PrimesController.java
@@ -7,15 +7,15 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class PrimesController {
     @PostMapping(path = "/primes")
     @ResponseBody
     public ResponseEntity<PrimeResponse> primes(@Valid @RequestBody PrimeRequest primeRequest){
-        long[] primesArray = {2};
+        int[] primesArray = {2};
+        List<Integer> primesList = findPrimeNumbers(primeRequest.getNumber());
         PrimeResponse primeResponse = new PrimeResponse(primesArray);
         return ResponseEntity.ok()
                 .body(primeResponse);
@@ -31,5 +31,32 @@ public class PrimesController {
             errors.put(fieldName, errorMessage);
         });
         return errors;
+    }
+
+
+    // generating the list of prime numbers from 2 to the given number
+    // using the Sieve of Eratosthenes algorithm
+    public static List<Integer> findPrimeNumbers(int n) {
+        // initialize the array with "true", index denotes the numbers from 0 to n.
+        boolean[] primes = new boolean[n + 1];
+        Arrays.fill(primes, true);
+
+        //loop from 2 to x until x*x becomes greater than n
+        for (int i = 2; i * i < n; i++) {
+            // process if the number is not already marked
+            if (primes[i]) {
+                // find the multiples and mark them as false
+                for (int j = i * i; j <= n; j += i)
+                    primes[j] = false;
+            }
+        }
+
+        // populate the list of prime numbers from the array and return it
+        List<Integer> primeNumbers = new ArrayList<>();
+        for (int i = 2; i <= n; i++) {
+            if (primes[i])
+                primeNumbers.add(i);
+        }
+        return primeNumbers;
     }
 }
