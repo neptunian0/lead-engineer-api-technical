e5cf8c9e29e5b032feb58f374ee39d5fd7128c5f
Lloyd Tucker <lloyd.tucker@bluebank.io>
2021-04-04 23:47:56 +0100

    First integration test

---
+++ src/test/java/com/natwest/restservice/integration/PrimesControllerIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.natwest.restservice.integration;
+
+import com.natwest.restservice.RestServiceApplication;
+import com.natwest.restservice.controller.PrimesController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PrimesController.class)
+public class PrimesControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    // write test cases here
+    @Test
+    public void givenEmployees_whenGetEmployees_thenStatus200() throws Exception {
+        mvc.perform(get("/api/primes/2")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().string("{\"algorithm\":\"eratosthenes\",\"primes\":[2]}"));
+    }
+}
