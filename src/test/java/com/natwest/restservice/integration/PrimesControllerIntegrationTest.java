5b10feb295888bf6f955a049741b6b97a792290b
Lloyd Tucker <lloyd.tucker@bluebank.io>
2021-04-05 12:37:40 +0100

    Remove MVC integration tests and use @SpringBootTest

--- src/test/java/com/natwest/restservice/integration/PrimesControllerIntegrationTest.java
+++ src/test/java/com/natwest/restservice/integration/PrimesControllerIntegrationTest.java
@@ -1,41 +1,34 @@
 package com.natwest.restservice.integration;
 
-import com.natwest.restservice.RestServiceApplication;
-import com.natwest.restservice.controller.PrimesController;
 import com.natwest.restservice.dto.PrimeResponse;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.Result;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 
-//import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
-//import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
-import static net.bytebuddy.matcher.ElementMatchers.is;
-import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(PrimesController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class PrimesControllerIntegrationTest {
-    Integer[] firstPrime = {2},
+    @LocalServerPort private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    Integer[] emptyPrimes = {},
+            firstPrime = {2},
             firstTenPrimes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29},
             firstHundredPrimes = {
                     2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
@@ -50,45 +43,89 @@ public class PrimesControllerIntegrationTest {
                     467, 479, 487, 491, 499, 503, 509, 521, 523, 541
             };
 
-    PrimeResponse firstPrimeEratosthenes = new PrimeResponse(
-                "eratosthenes", new ArrayList<>(Arrays.asList(firstPrime))),
-        firstTenPrimesAtkin = new PrimeResponse(
-                "atkin", new ArrayList<>(Arrays.asList(firstTenPrimes))),
-        firstHundredPrimesSundaram = new PrimeResponse(
-                "sundaram", new ArrayList<>(Arrays.asList(firstHundredPrimes)));
-
-    @Autowired
-    private MockMvc mvc;
-
     // write test cases here
     @Test
     public void given2_whenGetPrimes_thenGet2() throws Exception {
-        mvc.perform(get("/api/primes/2")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content()
-                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(content().string("{\"algorithm\":\"eratosthenes\",\"primes\":[2]}"));
+        ResponseEntity<PrimeResponse> response =
+                this.restTemplate.getForEntity(getPrimesURL("/api/primes/2"), PrimeResponse.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("application/json", response.getHeaders().getContentType().toString());
+        assertEquals("eratosthenes", response.getBody().getAlgorithm());
+        assertArrayEquals(firstPrime, response.getBody().getPrimes().toArray());
     }
 
     @Test
-    public void given30_andAtkin_whenGetPrimes_thenGetFirst10Primes() throws Exception {
-        mvc.perform(get("/api/primes/30")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content()
-                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].algorithm").value(is(firstTenPrimesAtkin.getAlgorithm())));
+    public void givenNeg5_whenGetPrimes_thenGetEmptyList() throws Exception {
+        ResponseEntity<PrimeResponse> response =
+                this.restTemplate.getForEntity(getPrimesURL("/api/primes/-5"), PrimeResponse.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("application/json", response.getHeaders().getContentType().toString());
+        assertEquals("eratosthenes", response.getBody().getAlgorithm());
+        assertArrayEquals(emptyPrimes, response.getBody().getPrimes().toArray());
     }
 
     @Test
-    public void given541_andSundaram_whenGetPrimes_thenGetFirst100Primes() throws Exception {
-        mvc.perform(get("/api/primes/30")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content()
-                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect((ResultMatcher) jsonPath("$[0].name", is(firstHundredPrimesSundaram.getAlgorithm())));
+    public void given30_andAtkin_whenGetPrimes_thenGetFirstTen_andAtkin() throws Exception {
+        ResponseEntity<PrimeResponse> response =
+                this.restTemplate.getForEntity(getPrimesURL("/api/primes/30?algorithm=atkin"), PrimeResponse.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("application/json", response.getHeaders().getContentType().toString());
+        assertEquals("atkin", response.getBody().getAlgorithm());
+        assertArrayEquals(firstTenPrimes, response.getBody().getPrimes().toArray());
+    }
+
+    @Test
+    public void given541_andSundaram_whenGetPrimes_thenGetFirstHundred_andSundaram() throws Exception {
+        ResponseEntity<PrimeResponse> response =
+                this.restTemplate.getForEntity(
+                        getPrimesURL("/api/primes/541?algorithm=sundaram"),
+                        PrimeResponse.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("application/json", response.getHeaders().getContentType().toString());
+        assertEquals("sundaram", response.getBody().getAlgorithm());
+        assertArrayEquals(firstHundredPrimes, response.getBody().getPrimes().toArray());
+    }
+
+    @Test
+    public void given30_andRandomCaseEratosthenes_whenGetPrimes_thenGetFirst10_andEratosthenes() throws Exception {
+        ResponseEntity<PrimeResponse> response =
+                this.restTemplate.getForEntity(
+                        getPrimesURL("/api/primes/30?algorithm=EratosThenes"),
+                        PrimeResponse.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("application/json", response.getHeaders().getContentType().toString());
+        assertEquals("eratosthenes", response.getBody().getAlgorithm());
+        assertArrayEquals(firstTenPrimes, response.getBody().getPrimes().toArray());
+    }
+
+    @Test
+    public void givenBadAlgorithm_whenGetPrimes_thenGetAlgorithmNotFoundException() throws Exception {
+        ResponseEntity<String> response =
+                this.restTemplate.getForEntity(
+                        getPrimesURL("/api/primes/541?algorithm=eratothenes"),
+                        String.class);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals("Algorithm not found", response.getBody());
+    }
+
+    @Test
+    public void givenXMLRequest_whenGetPrimes_thenGetXML() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_XML));
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+        ResponseEntity<PrimeResponse> response =
+                restTemplate.exchange(
+                        getPrimesURL("/api/primes/541?algorithm=atkin"),
+                        HttpMethod.GET,
+                        httpEntity,
+                        PrimeResponse.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertTrue(response.getHeaders().getContentType().toString().contains("application/xml"));
+        assertArrayEquals(firstHundredPrimes, response.getBody().getPrimes().toArray());
+    }
+
+    private String getPrimesURL(String path){
+        return String.format("http://localhost:%d/%s", port, path);
     }
 }
