3cc3248e1032fa8da2a0b4b446b1c18fca27ab59
Lloyd Tucker <lloyd.tucker@bluebank.io>
2021-04-05 15:45:40 +0100

    Add custom swagger information

--- src/main/java/com/natwest/restservice/SwaggerConfig.java
+++ src/main/java/com/natwest/restservice/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.natwest.restservice;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,11 +14,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
                 .select()
                 .paths(PathSelectors.any())
                 .build();
     }
 
+    private ApiInfo getApiInfo(){
+        return new ApiInfoBuilder()
+                .title("Prime Numbers API")
+                .version("1.0")
+                .description("Lead Developer API technical exercise")
+                .contact(new Contact("Lloyd Tucker", null, "lloyd.tucker@natwest.com"))
+                .build();
+    }
 }
